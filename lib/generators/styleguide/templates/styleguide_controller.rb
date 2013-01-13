ab3601689ee7898eb62efe63679b520d3b24b488
Joe Nelson <cred+github@begriffs.com>
2013-01-13 15:28:51 -0600

    Detect markup types, and add border to help determine padding

--- lib/generators/styleguide/templates/styleguide_controller.rb
+++ lib/generators/styleguide/templates/styleguide_controller.rb
@@ -5,13 +5,16 @@ class StyleguideController < ApplicationController
     widget_files = Dir.glob('app/views/styleguide/widgets/_*.html*')
 
     @widgets = widget_files.reduce([]) do |widgets, filename|
+      lang = filename.match(/haml$/) ? 'haml' : 'markup'
+
       name = File.basename(filename)
                  .sub(/.html.*/, '')
                  .sub(/^_/, '')
 
       widgets << { :name       => name,
                    :filename   => filename,
-                   :contents   => File.read(filename) }
+                   :contents   => File.read(filename),
+                   :lang       => lang }
       widgets
     end
   end
