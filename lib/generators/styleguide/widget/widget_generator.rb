94052213679e1f12de21809d4d7921f849e8569f
Josh Hoff <randomenduser@gmail.com>
2013-05-03 12:40:30 -0500

    only use supported templates

--- lib/generators/styleguide/widget/widget_generator.rb
+++ lib/generators/styleguide/widget/widget_generator.rb
@@ -9,8 +9,13 @@ module Styleguide
       desc "Creates a widget in your styleguide"
 
       def create_widget
+        supported_template_engines = [:haml, :erb]
         destination_name = name.gsub(/-/, '_').parameterize(sep = '_')
         template_engine = Rails.configuration.generators.options[:rails][:template_engine]
+
+        # use erb if we don't have a template for the engine currently in use
+        template_engine = :erb if !supported_template_engines.include? template_engine
+
         copy_file "widget.html.#{template_engine}", "app/views/styleguide/widgets/_#{destination_name}.html.#{template_engine}"
       end
     end
